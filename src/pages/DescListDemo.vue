2d038ccccaf4c0b1402f91729a5ffe89164e7ae3
yanhan <yanhan569046480@gmail.com>
2025-09-29 21:44:16 +0800

    将主页面的组件抽成组件

---
+++ src/pages/DescListDemo.vue
@@ -0,0 +1,88 @@
+<template>
+  <el-scrollbar height="calc(100vh - 160px)">
+    <el-radio-group v-model="size">
+      <el-radio value="large">Large</el-radio>
+      <el-radio value="default">Default</el-radio>
+      <el-radio value="small">Small</el-radio>
+    </el-radio-group>
+
+    <el-descriptions class="margin-top" title="With border" :column="3" :size="size" border>
+      <template #extra>
+        <el-button type="primary">Operation</el-button>
+      </template>
+      <el-descriptions-item>
+        <template #label>
+          <div class="cell-item">
+            <el-icon :style="iconStyle">
+              <user />
+            </el-icon>
+            Username
+          </div>
+        </template>
+        kooriookami
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template #label>
+          <div class="cell-item">
+            <el-icon :style="iconStyle">
+              <iphone />
+            </el-icon>
+            Telephone
+          </div>
+        </template>
+        18100000000
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template #label>
+          <div class="cell-item">
+            <el-icon :style="iconStyle">
+              <location />
+            </el-icon>
+            Place
+          </div>
+        </template>
+        Suzhou
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template #label>
+          <div class="cell-item">
+            <el-icon :style="iconStyle">
+              <tickets />
+            </el-icon>
+            Remarks
+          </div>
+        </template>
+        <el-tag size="small">School</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item>
+        <template #label>
+          <div class="cell-item">
+            <el-icon :style="iconStyle">
+              <office-building />
+            </el-icon>
+            Address
+          </div>
+        </template>
+        No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
+      </el-descriptions-item>
+    </el-descriptions>
+  </el-scrollbar>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import type { ComponentSize } from 'element-plus'
+
+const size = ref<ComponentSize>('default')
+
+const iconStyle = computed(() => {
+  const marginMap = {
+    large: '8px',
+    default: '6px',
+    small: '4px',
+  }
+  return {
+    marginRight: marginMap[size.value as keyof typeof marginMap] || marginMap.default,
+  }
+})
+</script>
---
+++ src/pages/TableDemo.vue
@@ -0,0 +1,146 @@
+<template>
+  <el-scrollbar height="calc(100vh - 160px)">
+    <el-table ref="multipleTableRef" :data="tableData" @selection-change="handleSelectionChange" style="width: 100%">
+      <el-table-column type="selection" :selectable="selectable" width="35" />
+      <el-table-column prop="date" label="Date" />
+      <el-table-column label="Delivery Info">
+        <el-table-column prop="name" label="Name" />
+        <el-table-column label="Address Info">
+          <el-table-column prop="state" label="State" />
+          <el-table-column prop="city" label="City" />
+          <el-table-column prop="address" label="Address" />
+          <el-table-column prop="zip" label="Zip" />
+        </el-table-column>
+      </el-table-column>
+      <el-table-column label="Operations">
+        <template #default="scope">
+          <el-button size="small" @click="handleEdit(scope.$index, scope.row)">
+            Edit
+          </el-button>
+          <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">
+            Delete
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-scrollbar>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { TableInstance } from 'element-plus'
+
+const multipleTableRef = ref<TableInstance>()
+
+interface User {
+  date: string
+  name: string
+  state: string
+  city: string
+  address: string
+  zip: string
+}
+
+const multipleSelection = ref<User[]>([])
+const selectable = (row: User) => !["Tom", "Jerry"].includes(row.name)
+const handleSelectionChange = (val: User[]) => {
+  multipleSelection.value = val
+}
+const handleEdit = (index: number, row: User) => {
+  console.log(index, row)
+}
+const handleDelete = (index: number, row: User) => {
+  console.log(index, row)
+}
+
+const tableData: User[] = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Jerry',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom2',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom3',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-08',
+    name: 'Tom4',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-06',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+  {
+    date: '2016-05-07',
+    name: 'Tom',
+    state: 'California',
+    city: 'Los Angeles',
+    address: 'No. 189, Grove St, Los Angeles',
+    zip: 'CA 90036',
+  },
+]
+</script>
